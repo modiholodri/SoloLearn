8583a3dcea3d48a6fc633bd48eb512ad8ccffecb
Modi777 <reinhold.lauer@gmail.com>
2019-01-06 10:54:40 +0700

    Updated Browser Me

--- BrowserMeInclude.js
+++ BrowserMeInclude.js
@@ -1,4 +1,4 @@
-// Browser Me - 2018-07-28 - Created by Modi
+// Browser Me - 2018-07-31 - Created by Modi
 /******************************************************
 Run a SoloLearn code in the Browser.
 *******************************************************/
@@ -23,11 +23,11 @@ function __log( message ) {
 }
 
 // download the combined code so that it can be executed
-function __saveCodeAsBlob() {
+function __saveCodeAsBlob( title ) {
     var codeAsBlob = new Blob([ __htmlCode ], { type: 'html' });
     
     var downloadLink = document.createElement("a");
-    downloadLink.download = "SoloLearnCode.html";
+    downloadLink.download = title + ".html";
     downloadLink.innerHTML = "Download File";
     if (window.webkitURL !== null) { // Chrome allows the link to be clicked without actually adding it to the DOM.
         downloadLink.href = window.webkitURL.createObjectURL( codeAsBlob );
@@ -43,7 +43,7 @@ function __saveCodeAsBlob() {
 }
 
 // store the codes and combine into a single HTML if all loaded
-function __storeCode( language, sourceCode ) {
+function __storeCode( language, sourceCode, title ) {
     if ( language == 'markdown' ) { // show the about
         $( '#aboutArea' ).text( sourceCode );
         return;
@@ -60,12 +60,12 @@ function __storeCode( language, sourceCode ) {
         __htmlCode = __htmlCode.replace( '<\/head>', '<style>\n\n' + __cssCode + '\n\n<\/style><\/head>' );
         __htmlCode = __htmlCode.replace( '<\/body>', '<script>\n\n' + __jsCode + '\n\n<\/script><\/body>' );
     
-        __saveCodeAsBlob();  // save the blob
+        __saveCodeAsBlob( title );  // save the blob
     }
 }
 
 // load the code in the corresponding selection    
-function __getCode( language, codePageOnSoloLearn ) { 
+function __getCode( language, codePageOnSoloLearn, title ) { 
     __log( ' Requesting ' + language + ' code');
     
     // handle JavaScript special case
@@ -79,7 +79,7 @@ function __getCode( language, codePageOnSoloLearn ) {
         success: function( data, status, xhr ) { // succeeded with the request
             if ( status == 'success' ) {
                 var sourceCode = $( data ).find( codeLocation ).attr( 'value' ); 
-                __storeCode( language, sourceCode );
+                __storeCode( language, sourceCode, title );
             }
         },
         error: function( error ) {  // log any error
@@ -90,7 +90,7 @@ function __getCode( language, codePageOnSoloLearn ) {
                 success: function( data, status, xhr ) { // succeeded with the request
                     if ( status == 'success' ) {
                         var sourceCode = $( data ).find( codeLocation ).attr( 'value' ); 
-                        __storeCode( language, sourceCode );
+                        __storeCode( language, sourceCode, title );
                     }
                 },
                 error: function( error ) { // log any error maybe later
@@ -102,10 +102,11 @@ function __getCode( language, codePageOnSoloLearn ) {
 }
 
 // load the codes from the SoloLearn codepage
-function __loadCodes ( sCodePage ) {
-    __getCode( 'html', sCodePage + '#html' );
-    __getCode( 'css', sCodePage + '#css' );
-    __getCode( 'js', sCodePage + '#js' );
+function __loadCodes ( sCodePage, title ) {
+    __htmlCode = __cssCode = __jsCode = ''; // reset the global variables
+    __getCode( 'html', sCodePage + '#html', title );
+    __getCode( 'css', sCodePage + '#css', title );
+    __getCode( 'js', sCodePage + '#js', title );
 }
 
 function __logLocationInfo ( ) {
@@ -124,14 +125,14 @@ $( document ).ready( function( ) {
     else if ( location.hostname == 'code.sololearn.com' ) { // if the code is running in the browser under SoloLearn
         if ( confirm ( 'I am running in your browser under SoloLearn but would like to be fullscreen...\n' +
                 'Can you launch me after I have downloaded me?' ) ) {
-            __loadCodes( location.href );
+            __loadCodes( location.href, 'Browser Me Include Itself' );
         }
     }
     else if ( location.protocol == 'https:' ) { // if the code is running in the browser downloaded from the web
-        alert ( 'Thanks a lot for downloading me from the web running me in fullscreen mode!... ;-)' );
+        alert ( 'Thanks a lot for downloading me from the web running me in fullscreen mode!\nYou are my favorite... ;-)' );
     }
     else if ( location.protocol == 'file:' ) { // if the code is running in the browser in a downloaded file
-        alert ( 'Thanks a lot for downloading me to a file and running me in fullscreen mode!... ;-)' );
+        alert ( 'Thanks a lot for downloading me to a file and running me in fullscreen mode!\nYou are my favorite... ;-)' );
     }
     else { // no clue where the code is running
         alert ( 'I have no clue where I am running...\n' +
